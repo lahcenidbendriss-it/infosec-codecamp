6ed94b505e73e51b91b9376a84415ef32f1afd15
lahcen idbendriss <lahcenidbendriss@gmail.com>
2024-08-11 16:07:22 +0100

    oject helmet all

--- myApp.js
+++ myApp.js
@@ -6,24 +6,36 @@ const app = express();
 
 
 
-app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
-app.use(helmet.ieNoOpen());
+// app.use(helmet.hidePoweredBy());
+// app.use(helmet.frameguard({action: 'deny'}));
+// app.use(helmet.xssFilter());
+// app.use(helmet.noSniff());
+// app.use(helmet.ieNoOpen());
 
-const timeInSeconds = 90 * 24 * 60 * 60;
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+// const timeInSeconds = 90 * 24 * 60 * 60;
+// app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 
-app.use(helmet.dnsPrefetchControl());
-app.use(helmet.noCache());
-app.use(helmet.contentSecurityPolicy({
-  directives : {
-    defaultSrc : ["'self'"],
-  scriptSrc : ["'self'", 'trusted-cdn.com'],
-  }
+// app.use(helmet.dnsPrefetchControl());
+// app.use(helmet.noCache());
+// app.use(helmet.contentSecurityPolicy({
+//   directives : {
+//     defaultSrc : ["'self'"],
+//   scriptSrc : ["'self'", 'trusted-cdn.com'],
+//   }
 
   
+// }));
+
+
+app.use(helmet({
+  contentSecurityPolicy : {
+      directives : {
+        defaultSrc : ["'self'"],
+      scriptSrc : ["'self'", 'trusted-cdn.com'],
+      }
+
+},
+noCache : true
 }))
 
 
